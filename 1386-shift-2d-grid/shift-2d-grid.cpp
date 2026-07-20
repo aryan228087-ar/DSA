7cf02571480ec0a4f768d238c0ad4a1adb558625
aryan228087-ar <aryan228087@gmail.com>
2026-07-20 19:21:39 +0530

    Time: 4 ms (30.98%) | Memory: 19.4 MB (14.15%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.cpp
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -17,7 +17,8 @@ public:
         int idx = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                arr[i][j] = nums[idx++];
+                arr[i][j] = nums[idx];
+                idx++;
             }
         }
         return arr;
