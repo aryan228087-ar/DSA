1ac01e642ba12be319b27aaadb2e94913334f552
aryan228087-ar <aryan228087@gmail.com>
2026-07-20 19:21:15 +0530

    Time: 2 ms (49.52%) | Memory: 19.3 MB (14.15%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& arr, int k) {
+        int m = arr.size();
+        int n = arr[0].size();
+        //Treat the matrix as 1d array
+        vector<int> nums;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                nums.push_back(arr[i][j]);
+            }
+        }
+        int total = nums.size();
+        k %= total;
+        rotate(nums.begin(),nums.end()-k,nums.end());
+        //put elements back to arr
+        int idx = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j] = nums[idx++];
+            }
+        }
+        return arr;
+    }
+};
