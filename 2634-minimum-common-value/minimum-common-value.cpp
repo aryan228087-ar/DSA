995629c35a8aa1367b8499926f667d7a9984ecfc
aryan228087-ar <aryan228087@gmail.com>
2026-07-25 10:26:47 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (56.67%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int getCommon(vector<int>& arr1, vector<int>& arr2) {
+        int n1 = arr1.size();
+        int n2 = arr2.size();
+        int i = 0;
+        int j = 0;
+        while(i < n1 && j < n2){
+            if(arr1[i] == arr2[j]) return arr1[i];
+            else if(arr1[i] < arr2[j]) i++;
+            else j++;
+        }
+        return -1;
+    }
+};
