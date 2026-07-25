3381ee35edc2585905b4e5a5948e50e66f975715
aryan228087-ar <aryan228087@gmail.com>
2026-07-25 10:11:07 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (25.18%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -2,23 +2,45 @@ class Solution {
 public:
     int maximumCount(vector<int>& arr) {
         int n = arr.size();
-        int negcount = 0;
-        int poscount = 0;
-        for(int i=0;i<n;i++){
-            if(arr[i] < 0) negcount++;
-            else if(arr[i] == 0) continue;
-            else poscount++;
+        int lo = 0;
+        int hi = n-1;
+        //finding first non negative
+        int firstnonnegative = n;
+        while(lo <= hi){
+            int mid = lo + (hi-lo)/2;
+            if(arr[mid] >= 0){
+                firstnonnegative = mid;
+                hi = mid-1;
+            }
+            else lo = mid+1;
         }
-        return max(poscount,negcount);
+        int negcount = firstnonnegative;//because the count starts from 0
+        //finding first positive no (>0)
+        int firstpositive = n;
+        lo = 0;
+        hi = n-1;
+        while(lo <= hi){
+            int mid = lo + (hi-lo)/2;
+            if(arr[mid] > 0){
+                firstpositive = mid;
+                hi = mid-1;
+            }
+            else lo = mid+1;
+        }
+        int poscount = n - firstpositive;
+        return max(negcount,poscount);
     }
 };
 
+
+
+
 // int n = arr.size();
-//         int lo = 0;
-//         int hi = n-1;
-//         while(lo <= hi){
-//             int mid = lo + (hi-lo)/2;
-//             if(arr[mid] == 0){
-//                 if(mid > 0 && arr[mid-1] == 0)
-//             }
+//         int negcount = 0;
+//         int poscount = 0;
+//         for(int i=0;i<n;i++){
+//             if(arr[i] < 0) negcount++;
+//             else if(arr[i] == 0) continue;
+//             else poscount++;
 //         }
+//         return max(poscount,negcount);
