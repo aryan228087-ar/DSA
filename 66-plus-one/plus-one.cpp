2fd615d6f8b771e6dc98fb2a039d124170b73bf4
aryan228087-ar <aryan228087@gmail.com>
2026-07-31 12:28:22 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (28.45%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -3,17 +3,17 @@ public:
     vector<int> plusOne(vector<int>& arr) {
         int n = arr.size();
         vector<int> ans;
-        if(n==1){
-            if(arr[0] == 9){
-                ans.push_back(1);
-                ans.push_back(0);
-                return ans;
-            }
-            else{
-                ans.push_back(arr[0] + 1);
-                return ans;
-            }
-        }
+        // if(n==1){
+        //     if(arr[0] == 9){
+        //         ans.push_back(1);
+        //         ans.push_back(0);
+        //         return ans;
+        //     }
+        //     else{
+        //         ans.push_back(arr[0] + 1);
+        //         return ans;
+        //     }
+        // }
         ans = arr;
         for(int i=n-1;i>=0;i--){
            if(ans[i] < 9){
