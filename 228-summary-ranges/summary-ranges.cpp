aa32e792247a7df3d5d149178af06bcdd11b0eff
aryan228087-ar <aryan228087@gmail.com>
2026-08-01 23:04:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (41.03%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& arr) {
+        int n = arr.size();
+        vector<string> ans;
+        for(int i=0;i<n;i++){
+            int start = arr[i];
+            while(i+1 < n && arr[i+1] == arr[i] + 1){
+                i++;
+            }
+            int end = arr[i];
+            if(start == end) ans.push_back(to_string(start));
+            else ans.push_back(to_string(start) + "->" + to_string(end));
+        }
+        return ans;
+    }
+};
