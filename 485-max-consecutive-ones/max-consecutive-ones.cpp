0764e8ca41dd0ad84449b4bfc593418d68ea2cfc
aryan228087-ar <aryan228087@gmail.com>
2026-08-03 00:25:42 +0530

    Time: 0 ms (100.00%) | Memory: 50 MB (94.13%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -5,13 +5,15 @@ public:
         int maxcount = 0;
         int count = 0;
         for(int i=0;i<n;i++){
-            if(arr[i] == 1){
-                count++;
+            if(arr[i] == 0){
+                
                 maxcount = max(maxcount,count);
+                count = 0;
             }
-            else count = 0;
+            else count++;
             
         }
+        maxcount = max(maxcount,count);
         return maxcount;
     }
 };
