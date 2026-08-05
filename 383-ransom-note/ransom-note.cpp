946ff90730699593b72ebee0ca6d605b1b81e320
aryan228087-ar <aryan228087@gmail.com>
2026-08-05 20:33:14 +0530

    Time: 4 ms (38.31%) | Memory: 11.9 MB (29.81%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> mp;
+        for(auto ch : magazine){
+            mp[ch]++;
+        }
+        for(char ch : ransomNote){
+            if(mp[ch] == 0){
+                return false;
+            }
+            mp[ch]--;
+        }
+        return true;
+    }
+};
