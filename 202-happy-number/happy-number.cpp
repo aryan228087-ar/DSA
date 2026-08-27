7d341145f51af59cdbc37789ca970cf7a4039c0c
Aryan Raj <aryan228087@gmail.com>
2026-08-27 19:59:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (82.45%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -12,9 +12,9 @@ public:
     bool isHappy(int n) {
         int slow = n;
         int fast = square(n); //you can call it as next number
-        while(fast != 1 && slow != fast){
-            slow = square(slow);
-            fast = square(square(fast));
+        while(fast != 1 && slow != fast){ //we did not found 1 and we did not detect a cycle
+            slow = square(slow);  //slow move by one step and fast moves by 2 step 
+            fast = square(square(fast)); // if there is a cycle then fast cant't reach to 1 and so fals
         }
         return fast == 1;
     }
